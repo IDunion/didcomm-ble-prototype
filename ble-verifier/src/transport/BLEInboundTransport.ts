735d9fa3f41c7df1d9120db9fc5fa98e42365f3a
Christian Bormann <ChristianCarl.Bormann@de.bosch.com>
2022-01-31 14:00:55 +0100

    Fix Inbound transport
    Signed-off-by: Christian Bormann <ChristianCarl.Bormann@de.bosch.com>

--- ble-verifier/src/transport/BLEInboundTransport.ts
+++ ble-verifier/src/transport/BLEInboundTransport.ts
@@ -1,14 +1,21 @@
 import type { Agent, InboundTransport, Logger } from '@aries-framework/core'
 import { AgentConfig } from '@aries-framework/core'
 
-export class BLEOutboundTransport implements InboundTransport {
+export class BLEInboundTransport implements InboundTransport {
     private agent!: Agent
     private logger!: Logger
+
+    private characteristic: string
+    public supportedSchemes: string[] = ['blue', 'ble']
+
+    public constructor(characteristic: string) {
+        this.characteristic = characteristic
+    }
 
     public async start(agent: Agent): Promise<void> {
         const agentConfig = agent.injectionContainer.resolve(AgentConfig)
         this.logger = agentConfig.logger
-        this.logger.debug('Starting BLE outbound transport')
+        this.logger.debug('Starting BLE inbound transport')
 
     }
 
