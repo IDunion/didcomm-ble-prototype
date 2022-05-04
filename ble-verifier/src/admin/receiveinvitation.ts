ff4258cf23431d67dd01e1595121df78614b5d58
Christian Bormann <ChristianCarl.Bormann@de.bosch.com>
2022-05-04 09:20:05 +0200

    Fix indeting in admin implementation
    Signed-off-by: Christian Bormann <ChristianCarl.Bormann@de.bosch.com>

--- ble-verifier/src/admin/receiveinvitation.ts
+++ ble-verifier/src/admin/receiveinvitation.ts
@@ -5,43 +5,43 @@ import { Agent, ConnectionInvitationMessage } from "@aries-framework/core";
 
 
 export class AdminReceiveInvitation implements AdminRoute {
-    private agent: Agent
-    private logger: TestLogger
+  private agent: Agent
+  private logger: TestLogger
 
-    constructor(logger: TestLogger, agent: Agent) {
-        this.agent = agent
-        this.logger = logger
-    }
+  constructor(logger: TestLogger, agent: Agent) {
+    this.agent = agent
+    this.logger = logger
+  }
 
-    register(express: Express) {
-        this.logger.debug('registering route for connection invitations')
+  register(express: Express) {
+    this.logger.debug('registering route for connection invitations')
 
-        express.post('/invitation', (req: Request, res: Response) => {
-            this.logger.debug('Got connection invitation')
-            const invitation = req.body
-            const connectionRecord  = this.agent.connections.receiveInvitation(invitation)
-            connectionRecord.then(record => {
-                this.logger.debug('Connection invitation accepted')
-                res.status(200).send('Invitation accepted')
-            }).catch(record => {
-                this.logger.error('Connection invitation invalid: ', record)
-                res.status(400).send('Invalid invitation')
-            })
+    express.post('/invitation', (req: Request, res: Response) => {
+      this.logger.debug('Got connection invitation')
+      const invitation = req.body
+      const connectionRecord = this.agent.connections.receiveInvitation(invitation)
+      connectionRecord.then(record => {
+        this.logger.debug('Connection invitation accepted')
+        res.status(200).send('Invitation accepted')
+      }).catch(record => {
+        this.logger.error('Connection invitation invalid: ', record)
+        res.status(400).send('Invalid invitation')
+      })
+    })
+
+    express.post('/invitationurl', (req: Request, res: Response) => {
+      this.logger.debug('Got connection invitation url: ', req.body)
+      const invitation = ConnectionInvitationMessage.fromUrl(req.body.url)
+      invitation.then(invitation => {
+        const connectionRecord = this.agent.connections.receiveInvitation(invitation)
+        connectionRecord.then(record => {
+          this.logger.debug('Connection invitation accepted')
+          res.status(200).send('Invitation accepted')
+        }).catch(record => {
+          this.logger.error('Connection invitation invalid: ', record)
+          res.status(400).send('Invalid invitation')
         })
-
-        express.post('/invitationurl', (req: Request, res: Response) => {
-            this.logger.debug('Got connection invitation url: ', req.body)
-            const invitation = ConnectionInvitationMessage.fromUrl(req.body.url)
-            invitation.then(invitation => {
-                const connectionRecord  = this.agent.connections.receiveInvitation(invitation)
-                connectionRecord.then(record => {
-                    this.logger.debug('Connection invitation accepted')
-                    res.status(200).send('Invitation accepted')
-                }).catch(record => {
-                    this.logger.error('Connection invitation invalid: ', record)
-                    res.status(400).send('Invalid invitation')
-                })
-            })
-        })
-    }
+      })
+    })
+  }
 }
--- ble-verifier/src/admin/webserver.ts
+++ ble-verifier/src/admin/webserver.ts
@@ -1,6 +1,6 @@
 import { Agent } from '@aries-framework/core'
 import express from 'express'
-import bodyParser, { BodyParser } from  'body-parser'
+import bodyParser, { BodyParser } from 'body-parser'
 import { TestLogger } from '../utils/logger'
 import type { AdminRoute } from './route'
 import { AdminReceiveInvitation } from './receiveinvitation'
@@ -9,35 +9,35 @@ import { AdminCreateInvitation } from './createinvitation'
 
 
 export class AdminWebServer {
-    private logger: TestLogger
-    private app: express.Express
-    private agent: Agent
+  private logger: TestLogger
+  private app: express.Express
+  private agent: Agent
 
-    constructor(logger: TestLogger, agent: Agent) {
-        this.logger = logger
-        this.agent = agent
+  constructor(logger: TestLogger, agent: Agent) {
+    this.logger = logger
+    this.agent = agent
 
-        // Create express app
-        const app = express()
-        app.use(bodyParser.json());
-        this.app = app
+    // Create express app
+    const app = express()
+    app.use(bodyParser.json());
+    this.app = app
 
-        // Regsiter default routes
-        const receiveInvitation = new AdminReceiveInvitation(logger, agent)
-        this.registerRoute(receiveInvitation)
-        const createInvitation = new AdminCreateInvitation(logger, agent)
-        this.registerRoute(createInvitation)
-        const connections = new AdminConnections(logger, agent)
-        this.registerRoute(connections)
-    }
+    // Regsiter default routes
+    const receiveInvitation = new AdminReceiveInvitation(logger, agent)
+    this.registerRoute(receiveInvitation)
+    const createInvitation = new AdminCreateInvitation(logger, agent)
+    this.registerRoute(createInvitation)
+    const connections = new AdminConnections(logger, agent)
+    this.registerRoute(connections)
+  }
 
-    public registerRoute(route: AdminRoute) {
-        route.register(this.app)
-    }
+  public registerRoute(route: AdminRoute) {
+    route.register(this.app)
+  }
 
-    public async listen(port: number) {
-        return this.app.listen(port, () => {
-            this.logger.info('Admin Server is running at port ' + port)
-        })
-    }
+  public async listen(port: number) {
+    return this.app.listen(port, () => {
+      this.logger.info('Admin Server is running at port ' + port)
+    })
+  }
 }
